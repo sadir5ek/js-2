f3a9e94ab24aad7683f0168bc83edac5fdacc5f7
sadir5ek <abdiraimovsadirbek@gmail.com>
2024-10-11 17:57:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// // const num = 33
+// // const num1 = 50
+// // //1650
+// // console.log(num * num1)
+
+// // const num3 = 50
+
+// // const num4 = 100
+// // const num2 = "200"
+// // //300
+// // console.log(num4 + );
+
+ const num = parseInt(prompt("сан жаз")) 
+ const num1 = parseInt(prompt("дагы сан жаз ")) 
+
+if  (num > num1) {
+    alert("Большее число:" + num)
+}else if(num1>num){
+    alert("Большее число:" +num1)
+}
+else  {
+    alert("Числа равны");
+}
